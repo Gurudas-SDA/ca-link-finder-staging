2547ff603637937772ad0b9e5306ce7639f187a4
Gurudas-SDA <135467969+Gurudas-SDA@users.noreply.github.com>
2026-07-24 19:40:25 +0300

    chore: cache-bust

--- sw-precache.js
+++ sw-precache.js
@@ -29,8 +29,8 @@ self.PRECACHE = [
   'guide/ru/index.html?v=eb6fe36b',
   'icon-192.png?v=fab41429',
   'icon-512.png?v=88196aa7',
-  'index.html?v=d0a8e736',
-  'js/app.js?v=615f5c5e',
+  'index.html?v=dafd14ce',
+  'js/app.js?v=57b229a9',
   'js/auth-modal.js?v=2ba0f1fc',
   'js/auth.js?v=a4f11a49',
   'js/config.js?v=732be8cf',
@@ -51,4 +51,4 @@ self.PRECACHE = [
   'js/vendor/xlsx.full.min.js?v=cc015130',
   'manifest.json?v=abe52cf2',
 ];
-self.SHELL_VERSION = '4e6556e07262';
+self.SHELL_VERSION = '4b4b2e04ca45';
